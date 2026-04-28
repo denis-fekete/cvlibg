bbccc4ac765251f05de1d6a729e9b57a6de9924e
Denis Fekete <xfeket01@vutbr.cz>
2026-04-28 15:40:34 +0200

    feat: downgrade kotlin version for compatibility with older version, added jitpack.yml for jitpack

--- bangapp/build.gradle.kts
+++ bangapp/build.gradle.kts
@@ -12,9 +12,6 @@ android {
     defaultConfig {
         applicationId = "com.fekete.bangdemo"
         minSdk = 29
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- cvlibg/build.gradle.kts
+++ cvlibg/build.gradle.kts
@@ -14,9 +14,6 @@ android {
 
     defaultConfig {
         minSdk = 29
-        targetSdk = 36
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -42,18 +39,14 @@ android {
 dependencies {
     // default
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.13.0")
-    implementation("androidx.databinding:viewbinding:9.2.0")
     testImplementation("junit:junit:4.13.2")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.9")
-    implementation("androidx.fragment:fragment:1.8.9")
 
     // detection
     implementation("org.opencv:opencv:4.12.0")
-    implementation("com.microsoft.onnxruntime:onnxruntime-android:latest.release")
+    implementation("com.microsoft.onnxruntime:onnxruntime-android:1.25.0")
 
     // coroutines and concurrency
     implementation("androidx.concurrent:concurrent-futures-ktx:1.3.0")
@@ -61,11 +54,25 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
     // json
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
     implementation("androidx.camera:camera-core:1.5.1")
     implementation("androidx.camera:camera-camera2:1.5.1")
     implementation("androidx.camera:camera-view:1.5.1")
     implementation("androidx.camera:camera-lifecycle:1.5.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.denis-fekete"
+                artifactId = "cvlibg"
+                version = "0.9.2"
+            }
+        }
+    }
 }
