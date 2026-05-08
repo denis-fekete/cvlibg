f8c57c9d2dc56e2e1afe352f01fbf439a0c8d2bd
Denis Fekete <xfeket01@vutbr.cz>
2026-05-08 09:58:22 +0200

    feat: added stoping of the camera in detailed analysis, preserve battery, updated CardDetail class for future expansion

--- bangapp/src/main/java/com/fekete/bangdemo/data/CardDetail.kt
+++ bangapp/src/main/java/com/fekete/bangdemo/data/CardDetail.kt
@@ -1,5 +1,6 @@
 package com.fekete.bangdemo.data
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /**
@@ -8,7 +9,7 @@ import kotlinx.serialization.Serializable
  *
  * @param id id used navigating between detail cards
  * @param title title/name of the card
- * @param description description of the card
+ * @param descriptionEN description of the card
  * @param links link of string values, these are translated into clickable links that move user to details page of
  * linked card
  * @param imagePath path to the image from the assets folder
@@ -17,9 +18,34 @@ import kotlinx.serialization.Serializable
  */
 @Serializable
 data class CardDetail(
-    val id: String,
-    val title: String,
-    val description: String,
+    val id: String = "",
+    val title: String = "",
+    val descriptionEN: String = "",
+    val descriptionSK: String = "",
+    val descriptionCZ: String = "",
     val links: List<String> = emptyList(),
-    val imagePath: String? = null
+    val imagePath: String? = null,
+    val totalInDeck: Int = 0,
+    val type: CardType = CardType.Other
 )
+
+@Serializable
+enum class CardType {
+    @SerialName("action")
+    Action,
+
+    @SerialName("effect")
+    Effect,
+
+    @SerialName("character")
+    Character,
+
+    @SerialName("role")
+    Role,
+
+    @SerialName("symbol")
+    Symbol,
+
+    @SerialName("other")
+    Other,
+}
